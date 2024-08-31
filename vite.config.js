15886dfdd21391d15c59f030dddd5fa28499f1ad
Luis Davi <luisdavi.contato@gmail.com>
2024-08-31 16:04:06 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
-import { resolve } from 'path'
+import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.js'),
+      name: "LaravueLib",
+      fileName: "laravue-lib",
+    },
+    
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "vue",
+        },
+      },
+    },
+  },
 });
